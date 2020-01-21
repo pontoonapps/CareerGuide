b458745c575d752da445d56f5721eea58fc793b5
Jacek Kopecky <jacek@jacek.cz>
2020-01-21 22:03:32 +0000

    trying more login through php
    giving up on that now because the PHP login page uses CSRF protection 
    and it doesn't feel secure to side-step it

--- maps-api/index.js
+++ maps-api/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 // const cookieParser = require('cookie-parser');
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const { URLSearchParams } = require('url');
+
 const config = require('./config');
 
 const rootApp = express();
@@ -16,6 +19,7 @@ app.get('/', msg('hi, testing'));
 // app.get('/env', msg(JSON.stringify(process.env)));
 // app.get('/cook', cookieParser(), showCookies);
 app.get('/is_logged_in', isLoggedIn);
+app.post('/login', bodyParser.json(), login);
 app.get('/count', sqlCount);
 
 rootApp.use(express.static('public'));
@@ -34,26 +38,61 @@ function msg(...message) {
 // }
 
 async function isLoggedIn(req, res) {
-  const fetchResp = await fetch(config.LOGIN_CHECK_URL, {
-    method: 'GET',
-    headers: {
-      cookie: req.get('cookie'),
-    },
-  });
-
-  if (!fetchResp.ok) {
-    res.json('error ' + fetchResp.status);
-    return;
-  }
-
   try {
+    const fetchResp = await fetch(config.LOGIN_CHECK_URL, {
+      method: 'GET',
+      headers: {
+        cookie: req.get('cookie'),
+      },
+    });
+
+    if (!fetchResp.ok) {
+      res.json({ error: fetchResp.status });
+      return;
+    }
+
     const loggedIn = await fetchResp.json();
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
     res.json(loggedIn);
   } catch (e) {
     res.json('exception ' + e.message);
   }
 }
 
+async function login(req, res) {
+  try {
+    if (!req.body || !req.body.email || !req.body.password) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const params = new URLSearchParams();
+    params.append('email', req.body.email);
+    params.append('password', req.body.password);
+
+    const fetchResp = await fetch(config.LOGIN_URL, {
+      method: 'POST',
+      body: params,
+      headers: {
+        referer: 'https://pontoonapps.com/',
+      },
+    });
+
+    if (!fetchResp.ok) {
+      res.json({ error: fetchResp.status });
+      return;
+    }
+
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+    res.json({
+      headers: Array.from(fetchResp.headers),
+      status: fetchResp.status,
+      body: await fetchResp.text(),
+    });
+  } catch (e) {
+    res.json('exception ' + e.message);
+  }
+}
 
 async function sqlCount(req, res) {
   const sql = await init();
