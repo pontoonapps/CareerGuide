aae6030d0bd99fef82cb823f9d9400b132ccb4b9
Jacek Kopecky <jacek@jacek.cz>
2020-01-21 23:22:45 +0000

    authenticate with ?apiKey and with basic auth
    basic auth user checked against the PHP database

--- maps-api/index.js
+++ maps-api/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-// const cookieParser = require('cookie-parser');
-const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
-const { URLSearchParams } = require('url');
+const basicAuth = require('express-basic-auth');
+const hashy = require('hashy');
 
 const config = require('./config');
 
@@ -13,13 +11,20 @@ const app = express.Router({
   strict: true,
 });
 
+const auth = basicAuth({
+  authorizer: checkDBUser,
+  authorizeAsync: true,
+});
+
 rootApp.use(config.DEPLOYMENT_ROOT || '/', app);
 
 app.get('/', msg('hi, testing'));
-// app.get('/env', msg(JSON.stringify(process.env)));
-// app.get('/cook', cookieParser(), showCookies);
-app.get('/is_logged_in', isLoggedIn);
-app.post('/login', bodyParser.json(), login);
+
+app.use(checkApiKey);
+app.get('/ping', msg('hi, api key working'));
+// app.get('/lasterr', (req, res) => res.json(globalLastError || 'none'));
+
+app.use(auth);
 app.get('/count', sqlCount);
 
 rootApp.use(express.static('public'));
@@ -29,83 +34,56 @@ function msg(...message) {
   message = message.join('\n');
   return (req, res) => {
     res.type('text/plain');
-    res.send(`${message}\n${new Date()}`);
+    res.send(`${message}\n${new Date()}\n`);
   };
 }
 
-// function showCookies(req, res) {
-//   res.json(req.cookies);
-// }
-
-async function isLoggedIn(req, res) {
-  try {
-    const fetchResp = await fetch(config.LOGIN_CHECK_URL, {
-      method: 'GET',
-      headers: {
-        cookie: req.get('cookie'),
-      },
-    });
-
-    if (!fetchResp.ok) {
-      res.json({ error: fetchResp.status });
-      return;
-    }
-
-    const loggedIn = await fetchResp.json();
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
-    res.json(loggedIn);
-  } catch (e) {
-    res.json('exception ' + e.message);
-  }
-}
-
-async function login(req, res) {
-  try {
-    if (!req.body || !req.body.email || !req.body.password) {
-      res.sendStatus(400);
-      return;
-    }
-
-    const params = new URLSearchParams();
-    params.append('email', req.body.email);
-    params.append('password', req.body.password);
-
-    const fetchResp = await fetch(config.LOGIN_URL, {
-      method: 'POST',
-      body: params,
-      headers: {
-        referer: 'https://pontoonapps.com/',
-      },
-    });
-
-    if (!fetchResp.ok) {
-      res.json({ error: fetchResp.status });
-      return;
-    }
-
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
-    res.json({
-      headers: Array.from(fetchResp.headers),
-      status: fetchResp.status,
-      body: await fetchResp.text(),
-    });
-  } catch (e) {
-    res.json('exception ' + e.message);
-  }
-}
-
 async function sqlCount(req, res) {
-  const sql = await init();
+  const sql = await initDB();
   const [rows] = await sql.query('SELECT COUNT(*) AS count FROM `users`');
   const count = Number(rows[0].count);
-  res.send(`hi, there are ${count} users as of ${new Date()}`);
+  res.send(`hi, there are ${count} users as of ${new Date()}\n`);
 }
 
-function init() {
+function initDB() {
   return mysql.createConnection({
     host: 'localhost',
     user: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_DATABASE,
   });
+}
+
+function checkApiKey(req, res, next) {
+  if (req.query.apiKey in config.RECOGNISED_API_KEYS) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+// let globalLastError;
+
+async function checkDBUser(user, pwd, cb) {
+  try {
+    // globalLastError = {};
+    const sql = await initDB();
+    const query = `SELECT hashed_password
+                   FROM users
+                   WHERE email = ?`;
+    const [rows] = await sql.query(query, [user]);
+    // globalLastError.rows = rows;
+    if (rows.length === 0) {
+      cb(null, false);
+      return;
+    }
+    const hash = rows[0].hashed_password;
+    // globalLastError.hash = hash;
+    const match = await hashy.verify(pwd, hash);
+    // globalLastError.match = match;
+    cb(null, match === true);
+  } catch (e) {
+    // globalLastError.exception = e;
+    cb(null, false);
+  }
 }
