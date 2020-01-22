8163c900e7557283ea6d77da79b32fe8db033a09
Jacek Kopecky <jacek@jacek.cz>
2020-01-22 23:17:58 +0000

    add skeleton for the API

--- maps-api/api.js
+++ maps-api/api.js
@@ -22,6 +22,10 @@ app.get('/ping', msg('hi, api key working'));
 app.use(auth);
 app.get('/testcount', testCount);
 
+app.post('/pins', express.json(), addPin);
+app.post('/pins/delete', express.json(), deletePin);
+app.get('/pins', getUserPins);
+
 // server functions
 
 function msg(...message) {
@@ -54,6 +58,30 @@ async function testCount(req, res, next) {
   try {
     const count = await db.testCount();
     res.send(`hi, there are ${count} users as of ${new Date()}\n`);
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function addPin(req, res, next) {
+  try {
+    res.sendStatus(501);
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function deletePin(req, res, next) {
+  try {
+    res.sendStatus(501);
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function getUserPins(req, res, next) {
+  try {
+    res.sendStatus(501);
   } catch (e) {
     next(e);
   }
