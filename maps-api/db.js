5ea5efb5d77f30e9f73fb192df543e61614d7e40
Jacek Kopecky <jacek@jacek.cz>
2020-01-28 23:29:29 +0000

    move to using one connection

--- maps-api/db.js
+++ maps-api/db.js
@@ -2,17 +2,15 @@ const mysql = require('mysql2/promise');
 const hashy = require('hashy');
 const config = require('./config');
 
-function initDB() {
-  return mysql.createConnection({
-    host: 'localhost',
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE,
-  });
-}
+const dbConn = mysql.createConnection({
+  host: 'localhost',
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_DATABASE,
+});
 
 async function testCount() {
-  const sql = await initDB();
+  const sql = await dbConn;
   const [rows] = await sql.query('SELECT COUNT(*) AS count FROM `users`');
   const count = Number(rows[0].count);
   return count;
@@ -20,7 +18,7 @@ async function testCount() {
 
 async function findUser(user, pwd) {
   try {
-    const sql = await initDB();
+    const sql = await dbConn;
     const query = `SELECT id, hashed_password
                    FROM users
                    WHERE email = ?`;
