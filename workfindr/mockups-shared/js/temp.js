9ff39856d357c144efda1616a7df2bc76390eed5
KaneSwartz <up941226@myport.ac.uk>
2020-04-09 15:54:32 +0100

    swipe list code draft and minor CSS fixes

--- workfindr/mockups-shared/js/temp.js
+++ workfindr/mockups-shared/js/temp.js
@@ -8,13 +8,43 @@ function getData() {
   const data = {};
   data.jobs = {
     type: 'jobs',
-    jobs: [{
+    jobList: [{
       ID: '1',
       title: 'Plumber',
       description: 'Red dungarees and hat included',
       image: 'img/tempImage.png',
     }, {
       ID: '2',
+      title: 'Programmer',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '3',
+      title: 'Plumber',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '4',
+      title: 'Programmer',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '5',
+      title: 'Plumber',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '6',
+      title: 'Programmer',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '7',
+      title: 'Plumber',
+      description: 'Light up keyboard provided',
+      image: 'img/tempImage.png',
+    }, {
+      ID: '8',
       title: 'Programmer',
       description: 'Light up keyboard provided',
       image: 'img/tempImage.png',
@@ -101,6 +131,8 @@ function getData() {
 
 // display pages
 
+// questionnaire review //
+
 function loadQuestionnaireReview() {
   const elem = document.querySelector('#questionnaire-template');
   for (const question of data.questionnaireAnswers.questions) {
@@ -112,35 +144,80 @@ function loadQuestionnaireReview() {
     const main = document.querySelector('main');
     main.appendChild(questionContainer);
   }
-  return 0; // Just to prevent undefined output in console.
 }
+
+// swipe history //
 
 function loadSwipeHistory() {
   const elem = document.querySelector('#swipe-history-temp');
-  const history = data.swipedOnJobs; // Can put function that returns answers from database here
-  history.jobs.forEach(job => {
-    console.log(job);
+  for (const job of data.swipedOnJobs.jobs) {
     const card = document.importNode(elem.content, true);
 
     card.querySelector('#swipeItemImg').setAttribute('src', job.image);
+    card.querySelector('#swipeItemImg').setAttribute('alt', job.title + 'image');
     card.querySelector('#listItemTitle').textContent = job.title;
     card.querySelector('#swipeItemDesc').textContent = job.description;
     card.querySelector('#swipeChoice').classList.add(job.swipe);
     card.querySelector('#swipeChoice').textContent = (job.swipe === 'liked' ? '👍' : '👎');
 
-    const main = document.getElementsByTagName('main')[0];
+    const main = document.querySelector('main');
     main.appendChild(card);
-  });
-  return 0; // Just to prevent undefined output in console.
+  }
 }
 
 function loadShortlist() {
-  // TODO: Swipe Page
-  return 0; // Just to prevent undefined output in console.
+  // TODO shortlist
 }
 
-function loadSwipePage() {
+// swipe page //
 
+let jobItemIndex = 1; // global variable to move through jobs (TODO there must be a better way)
+let jobsToSwipe = [];
+const swipes = [];
+
+function loadSwipePage() {
+  jobsToSwipe = getJobs(jobsToSwipe); // get a list of jobs
+  displayJobData(jobsToSwipe[0]); // display first job on page load
+
+  // add event listener to like dislike and show later buttons
+  // TODO create function for adding event listeners
+  // TODO create function for adding handles
+  document.querySelector('#btn-like').addEventListener('click', loadNextJob);
+  document.querySelector('#btn-showLater').addEventListener('click', loadNextJob);
+  document.querySelector('#btn-dislike').addEventListener('click', loadNextJob);
+}
+
+function loadNextJob() {
+  const getMoreJobsAt = 1; // when to get more jobs (in this case when only 1 left)
+
+  // add swipe choice to swipe array
+  const swipe = {};
+  swipe.type = event.target.textContent; // would a html attribute for swipe type be better
+  swipe.job = jobsToSwipe[jobItemIndex].ID;
+  swipes.push(swipe); // this would be sent to database to prevent jobs already swiped on being shown again
+
+  // display next job
+  displayJobData(jobsToSwipe[jobItemIndex]);
+  jobItemIndex += 1;
+
+  // if running low on jobs to swipe get more jobs
+  if (jobItemIndex > jobsToSwipe.length - getMoreJobsAt) {
+    getJobs(jobsToSwipe);
+  }
+}
+
+function displayJobData(job) {
+  document.querySelector('#title').textContent = job.title;
+  document.querySelector('#info-text').textContent = job.description;
+}
+
+function getJobs(jobsArray) {
+  const numJobsToAdd = 2; // in practice batches of 3 to 5 or 5 to 10 may be better
+  const jobsSwiped = jobsArray.length; // used get jobs which haven't already been shown, in practice the server would keep track of this
+  for (let i = 0; i < numJobsToAdd; i += 1) {
+    jobsArray.push(data.jobs.jobList[jobsSwiped + i]);
+  }
+  return jobsArray;
 }
 
 // start script (after page has loaded)
