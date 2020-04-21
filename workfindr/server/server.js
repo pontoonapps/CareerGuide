0365e37ace4aed2d2925415a4004728475a55128
KaneSwartz <up941226@myport.ac.uk>
2020-04-21 15:30:25 +0100

    swipe page working with server

--- workfindr/server/server.js
+++ workfindr/server/server.js
@@ -8,16 +8,68 @@ const app = express();
 
 // Make async later
 async function nextSwipeItem(req, res) {
+  if (itemsList.length === 0) {
+    refreshItemsList();
+  }
   await res.json(itemsList.pop());
 }
 
-// async function getShortlist(req, res) {
-//   await res.json();
-// }
+async function getSwiped(req, res) {
+  await res.sendStatus(404);
+}
+
+async function getQuestions(req, res) {
+  await res.sendStatus(404);
+}
+
+async function getShortlist(req, res) {
+  await res.json(slistJobs);
+}
+
+function ansQuestion(req, res) {
+  // get variables from req body
+  const choice = req.body.choice;
+  const qid = req.body.jobid; // qid short for questionid
+
+  // save question answer in DB
+  console.log('Choice:', choice);
+  console.log('questionID:', qid);
+  res.json();
+}
+
+function parseInput(req, res) {
+  // get variables from req body
+  const choice = req.body.choice;
+  const jobid = req.body.jobid;
+
+  // if choice is shortlist don't swipe job
+  if (choice !== 'shortlist') {
+    res.json(swipeJob(choice, jobid));
+  } else {
+    res.json(toggleShortlist(choice, jobid));
+  }
+}
+
+function swipeJob(choice, jobid) {
+  // save swipe in DB
+  console.log('Choice:', choice);
+  console.log('JobID:', jobid);
+}
+
+function toggleShortlist(choice, jobid) {
+  // save shortlist in DB
+  console.log('Toggle Choice:', choice);
+  console.log('Toggle JobID:', jobid);
+}
 
 // routes
 
-app.get('/user/next-item', asyncWrap(nextSwipeItem));
+app.get('/user/next-item', asyncWrap(nextSwipeItem)); // A+D
+app.get('/user/jobs/swiped', asyncWrap(getSwiped)); // F
+app.get('/user/questions', asyncWrap(getQuestions)); // G
+app.get('user/jobs?shortlist', asyncWrap(getShortlist)); // H
+app.post('/user/jobs', express.json(), asyncWrap(parseInput)); // B+C+I - How are we structuring this?
+app.post('/user/questions', express.json(), asyncWrap(ansQuestion)); // E
 
 // swipe page data
 
@@ -26,7 +78,6 @@ const testJobs = {
     id: 0,
     title: 'Plumber',
     description: 'Red dungarees and hat included',
-    image: 'img/tempImage.png',
   }, {
     id: 1,
     title: 'Programmer',
@@ -65,7 +116,32 @@ const testQuestions = {
   }],
 };
 
-const itemsList = testJobs.jobs.concat(testQuestions.questions);
+const slistJobs = {
+  jobs: [{
+    id: 0,
+    title: 'Teacher',
+    description: 'Can teach you how to do "X"',
+    image: 'img/tempimage.png',
+  }, {
+    id: 1,
+    title: 'Plumber',
+    description: 'Knows things about plumbing',
+    image: 'img/tempimage.png',
+  }, {
+    id: 2,
+    title: 'Priest',
+    description: 'Likes god',
+    image: 'img/tempimage.png',
+  }],
+};
+
+let itemsList;
+refreshItemsList(); // allows for inifinte swiping while testing swip page
+
+function refreshItemsList() {
+  itemsList = testJobs.jobs.concat(testQuestions.questions);
+}
+
 
 // wrap async function for express.js error handling
 function asyncWrap(f) {
