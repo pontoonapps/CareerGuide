0951896d05a488236dd7a1ae35b22cba2836c64a
KaneSwartz <up941226@myport.ac.uk>
2020-04-21 14:01:59 +0100

    server start and swipe page routes

--- workfindr/client/js/swipepage.js
+++ workfindr/client/js/swipepage.js
@@ -1,47 +1,26 @@
 // swipepage.js
 
-const swipes = [];
-
-const data = {
-  jobs: [{
-    id: 0,
-    title: 'Plumber',
-    description: 'Red dungarees and hat included',
-    image: 'img/tempImage.png',
-  }, {
-    id: 1,
-    title: 'Programmer',
-    description: 'Light up keyboard provided',
-    image: 'img/tempImage.png',
-  }, {
-    id: 2,
-    title: 'Con artist',
-    description: 'Steals your valuable possessions',
-    image: 'img/tempImage.png',
-  }, {
-    id: 3,
-    title: 'Teacher',
-    description: '"Might" teach you things',
-    image: 'img/tempImage.png',
-  }],
-};
-
-function getJob() {
-  // in application this will get a job from the server
-  return data.jobs[swipes.length % data.jobs.length]; // % used for inifnite job swiping
+async function getNextItem() {
+  const rawItem = await fetch('/user/next-item');
+  if (rawItem.ok) {
+    const item = await rawItem.json();
+    console.log(item);
+    return item;
+  }
+  return 'error connecting to server';
 }
 
 function submitSwipe(event) {
   // in application this will send the swipe data to the server
-  swipes.push({ type: event.target.textContent, job: data.jobs[swipes.length % data.jobs.length].id });
+  // swipes.push({ type: event.target.textContent, job: data.jobs[swipes.length % data.jobs.length].id });
+  return true;
 }
 
-function loadNextJob() {
+async function loadNextJob() {
   if (event.target.id !== undefined) { // if this is the first job being displayed, no swipe has been made
     submitSwipe(event);
   }
-  const job = getJob();
-  displayJob(job);
+  displayJob(await getNextItem());
 }
 
 function displayJob(job) {
