3bc56fd6d2fa2eb65a6dd8ad09a28c09628cfa62
KaneSwartz <up941226@myport.ac.uk>
2020-04-28 15:41:00 +0100

    questionnaire review and swipe history to server setup and housekeeping

--- workfindr/client/js/swipehistory.js
+++ workfindr/client/js/swipehistory.js
@@ -23,7 +23,8 @@ async function loadSwipHist() {
     cont.querySelector('.swipeItemDesc').textContent = job.description;
     cont.querySelector('.swipeChoice').classList.add(job.swipe);
     cont.querySelector('.swipeChoice').textContent = (job.swipe === 'liked' ? '👍' : '👎');
-    cont.querySelector('.swipeChoice').addEventListener('click', changeSwipe)
+    cont.querySelector('.swipeChoice').setAttribute('jobid', job.id); // TODO user dataspace not attributes
+    cont.querySelector('.swipeChoice').addEventListener('click', changeSwipe);
 
     const main = document.querySelector('main');
     main.appendChild(cont);
@@ -31,23 +32,51 @@ async function loadSwipHist() {
 }
 
 function changeSwipe() {
-  submitSwipeChangeToServer(); // overly verbose example function
-  switch(event.target.classList[1]) {
-    case 'liked':
-      event.target.classList.remove('liked');
-      event.target.classList.add('disliked');
-      event.target.textContent = '👎';
-      break;
-    case 'disliked':
-      event.target.classList.remove('disliked');
-      event.target.classList.add('liked');
-      event.target.textContent = '👍';
-      break;
+  const succSub = subSwipChange(); // overly verbose example function
+  if (succSub) {
+    switch (event.target.classList[1]) {
+      case 'liked':
+        event.target.classList.remove('liked');
+        event.target.classList.add('disliked');
+        event.target.textContent = '👎';
+        break;
+      case 'disliked':
+        event.target.classList.remove('disliked');
+        event.target.classList.add('liked');
+        event.target.textContent = '👍';
+        break;
+    }
+  } else {
+    // report error to user
   }
 }
 
-function submitSwipeChangeToServer() {
-  console.log('this function will submit the swipe change');
+async function subSwipChange() {
+  const usrInput = {};
+  usrInput.jobid = event.target.getAttribute('jobid');
+  switch (event.target.classList[1]) {
+    case 'liked':
+      usrInput.choice = 'disliked';
+      break;
+    case 'disliked':
+      usrInput.choice = 'liked';
+      break;
+  }
+  const response = await submitChange(usrInput);
+
+  if (!response.ok) {
+    console.log('error', response.statusText, 'cant change');
+  }
+  return (response.ok);
+}
+
+async function submitChange(usrInput) { // This is the same function as submitJInput on swipePage, repeated code??
+  const response = await fetch('/user/jobs', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(usrInput),
+  });
+  return response;
 }
 
 function loadPage() {
