66945b78fa45620753c4bb3a108f86ce48cfe17c
Jacek Kopecky <jacek@jacek.cz>
2020-05-15 13:47:50 +0100

    update db to include 'show later', other cleanups
    change questions.jobs_column to enum;
    in test data, add explicit IDs

--- workfindr/server/database/test-data.sql
+++ workfindr/server/database/test-data.sql
@@ -1,36 +1,36 @@
 INSERT INTO `pontoonapps_jobseeker`.`users`
-( first_name )
+( id, first_name )
 VALUES
-( 'jacek' ),
-( 'kane' ),
-( 'pontuz' ),
-( 'passive' );
+( 1, 'jacek' ),
+( 2, 'kane' ),
+( 3, 'pontuz' ),
+( 4, 'passive' );
 
 INSERT INTO `pontoonapps_workfindr2`.`categories`
-( title_en, icon_filename )
+( id, title_en, icon_filename )
 VALUES
-( 'jokes', 'jokes.png' ),
-( 'witze', 'jokes.jpg' );
+( 1, 'jokes', 'jokes.png' ),
+( 2, 'witze', 'jokes.jpg' );
 
 INSERT INTO `pontoonapps_workfindr2`.`jobs`
-( title_en, description_en, titre_fr, description_fr, category_id,
+( id, title_en, description_en, titre_fr, description_fr, category_id,
   teamwork, physical_activity, creativity, driving, travel, hours_flexibility, care_work, danger )
 VALUES
-( 'joker', 'here be jokes', 'ne parles', 'francaise', 1,
+( 1, 'joker', 'here be jokes', 'ne parles', 'francaise', 1,
   1, 2, 3, 1, 5, 6, 7, 8 ),
-( 'sadder', 'here be sadness', 'ne parles', 'francaise', 1,
+( 2, 'sadder', 'here be sadness', 'ne parles', 'francaise', 1,
   2, 3, 4, 2, 6, 7, 8, 9 ),
-( 'witzmacher', 'da ists lustig', 'ne parles', 'francaise', 2,
+( 3, 'witzmacher', 'da ists lustig', 'ne parles', 'francaise', 2,
   1, 1, 1, 2, 1, 1, 1, 1 ),
-( 'doofer', 'dummer', 'ne parles', 'francaise', 2,
+( 4, 'doofer', 'dummer', 'ne parles', 'francaise', 2,
   9, 9, 9, 1, 9, 9, 9, 9 );
 
 INSERT INTO `pontoonapps_workfindr2`.`likes`
 ( user_id, job_id, type )
 VALUES
 ( 1, 1, 'like' ),
-( 1, 2, 'dislike' ),
-( 1, 3, 'like' ),
+( 1, 2, 'show later' ),
+( 1, 3, 'dislike' ),
 ( 2, 3, 'like' ),
 ( 2, 4, 'dislike' ),
 ( 3, 4, 'dislike' );
@@ -45,10 +45,10 @@ VALUES
 
 
 INSERT INTO `pontoonapps_workfindr2`.`questions`
-( title_en, question_en, jobs_column )
+( id, title_en, question_en, jobs_column )
 VALUES
-( 'presence', 'you here?', 'teamwork' ),
-( 'engagement', 'you doing anything?', 'danger' );
+( 1, 'presence', 'you here?', 'teamwork' ),
+( 2, 'engagement', 'you doing anything?', 'danger' );
 
 
 INSERT INTO `pontoonapps_workfindr2`.`options`
