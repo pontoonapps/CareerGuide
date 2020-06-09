d0637e808837853269e0f43a3069dda30eee5045
JavaRip <up941226@myport.ac.uk>
2020-06-09 16:52:25 +0100

    improved query for get next question

--- workfindr/server/storage.js
+++ workfindr/server/storage.js
@@ -6,7 +6,6 @@ const sqlPromise = mysql.createConnection(config.mysql);
 async function swipedJobs(userId) {
   const sql = await sqlPromise;
 
-  // get swiped jobs (route B)
   const query = `
     SELECT
       jobs.id,
@@ -96,42 +95,36 @@ async function getNextJob(userId) {
 async function getNextQuestion(userId) {
   const sql = await sqlPromise;
 
-  const question = {};
   const query = `
-    SELECT qst.id, qst.title_en, qst.question_en, opt.option_number, opt.label_en
-    FROM (
-      SELECT id, title_en, question_en
-      FROM pontoonapps_workfindr2.questions
-      WHERE id NOT IN (
-        SELECT question_id
-        FROM pontoonapps_workfindr2.answers
-        WHERE user_id = ?
-      )
-      LIMIT 1
-    ) AS qst
-    JOIN pontoonapps_workfindr2.options AS opt ON qst.id = opt.question_id
-    ORDER BY qst.id, opt.option_number DESC`;
+    SELECT
+      questions.id,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          "label_en", options.label_en,
+          "option_number", options.option_number
+        )
+      ) AS options,
+      questions.question_en,
+      questions.title_en
+    FROM pontoonapps_workfindr2.questions
+    INNER JOIN pontoonapps_workfindr2.options
+      ON questions.id = options.question_id
+    LEFT JOIN pontoonapps_workfindr2.answers
+      ON questions.id = answers.question_id
+    WHERE questions.id NOT IN (
+      SELECT question_id
+      FROM pontoonapps_workfindr2.answers
+      WHERE user_id = ?
+    )
+    GROUP BY questions.id
+    LIMIT 1`;
   const [questionData] = await sql.query(query, userId);
-
-  if (questionData.length === 0) {
-    return;
-  }
-  // TODO remove formatting in JS with ARRAYGG
-  question.id = questionData[0].id;
-  question.question_en = questionData[0].question_en;
-  question.title_en = questionData[0].title_en;
-  question.options = [];
-  for (const row of questionData) {
-    question.options.push({
-      option_number: row.option_number,
-      label_en: row.label_en,
-    });
-  }
-  return question;
+  return questionData[0];
 }
 
 async function insertQuestionAnswer(ansData) {
   const sql = await sqlPromise;
+
   const query = `
     INSERT INTO pontoonapps_workfindr2.answers
       (user_id, question_id, option_number)
@@ -144,8 +137,7 @@ async function insertQuestionAnswer(ansData) {
 
 async function insertSwipe(swipeData) {
   const sql = await sqlPromise;
-  const userId = swipeData.userId;
-  const jobId = swipeData.itemId;
+
   let answer;
   switch (swipeData.choice) {
     case 'shortlist-add':
@@ -159,6 +151,7 @@ async function insertSwipe(swipeData) {
       answer = 3;
       break;
   }
+
   const query = `
     INSERT INTO pontoonapps_workfindr2.likes
       (user_id, job_id, type)
@@ -166,11 +159,12 @@ async function insertSwipe(swipeData) {
       (?, ?, ?)
     ON DUPLICATE KEY UPDATE
       type = ?`;
-  await sql.query(query, [userId, jobId, answer, answer]);
+  await sql.query(query, [swipeData.userId, swipeData.itemId, answer, answer]);
 }
 
 async function insertShortlist(swipeData) {
   const sql = await sqlPromise;
+
   const query = `
     INSERT INTO pontoonapps_workfindr2.shortlists
       (user_id, job_id)
@@ -183,6 +177,7 @@ async function insertShortlist(swipeData) {
 
 async function removeShortlist(swipeData) {
   const sql = await sqlPromise;
+
   const query = `
     DELETE FROM pontoonapps_workfindr2.shortlists
       WHERE
