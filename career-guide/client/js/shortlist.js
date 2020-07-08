8e6ae874a487de172a820d9922c917f24cd3b7ae
JavaRip <up941226@myport.ac.uk>
2020-07-08 16:51:41 +0100

    removed view more functionality from shortlist page

--- career-guide/client/js/shortlist.js
+++ career-guide/client/js/shortlist.js
@@ -30,84 +30,26 @@ async function loadShortlist() {
     jobContainer.querySelector('.list-item-title').textContent = job.title_en;
     jobContainer.querySelector('.job-desc').textContent = job.description_en;
     jobContainer.querySelector('.job-desc').dataset.fullDescription = job.description_en;
-    jobContainer.querySelector('.view-more').addEventListener('click', displayDetailedDesc);
-    jobContainer.querySelector('.view-more').dataset.jobid = job.id;
-    jobContainer.querySelector('.view-less').addEventListener('click', hideDetailedDesc);
-    jobContainer.querySelector('.view-less').dataset.jobid = job.id;
     jobContainer.querySelector('.remove-shortlist-item').addEventListener('click', removeShortlistItem);
     jobContainer.querySelector('.remove-shortlist-item').dataset.jobid = job.id;
 
     const listContainer = document.querySelector('#list-container');
     listContainer.appendChild(jobContainer);
   }
-
-  for (const description of document.querySelectorAll('.job-desc')) {
-    truncateOverflow(description.textContent, description);
-  }
 }
 
-function truncateOverflow(text, container) {
-  let cutText = text;
-  container.textContent = text;
-  while (container.clientHeight < container.scrollHeight && cutText.length > 0) {
-    cutText = cutText.slice(0, cutText.lastIndexOf(' ')); // remove last word so text is not cut mid word
-    container.textContent = cutText;
-  }
+async function removeShortlistItem(event) {
+  const delayPromise = timeoutDelay(BUTTON_DELAY);
 
-  if (cutText !== text) {
-    // remove two more words so elipses does not overflow
-    cutText = cutText.slice(0, cutText.lastIndexOf(' ', cutText.lastIndexOf(' ') - 1));
-    cutText += '…';
-    container.textContent = cutText;
-  }
-}
-
-function displayDetailedDesc(event) {
-  // reset all items to non expanded
-
-  for (const jobContainer of document.querySelectorAll('.list-item-container')) {
-    jobContainer.classList.remove('expanded'); // remove expanded class
-  }
-
-  for (const removeBtn of document.querySelectorAll('.remove-shortlist-item')) {
-    removeBtn.style.display = 'none'; // hide remove button
-  }
-
-  for (const viewLessBtn of document.querySelectorAll('.view-less')) {
-    viewLessBtn.style.display = 'none'; // hide view less button
-  }
-
-  for (const viewMoreBtn of document.querySelectorAll('.view-more')) {
-    viewMoreBtn.style.display = ''; // show view more button
-  }
-
-  // change target container to detailed view
-
-  // get required DOM elements
-  const viewMoreBtn = event.target;
-  const viewLessBtn = viewMoreBtn.nextElementSibling;
-  const removeBtn = viewLessBtn.nextElementSibling;
-  const buttonContainer = viewMoreBtn.parentNode;
-  const jobDescription = buttonContainer.previousElementSibling;
-  const listItemContainer = buttonContainer.parentNode;
-
-  // hide view more and show view less and remove button
-  removeBtn.style.display = '';
-  viewLessBtn.style.display = '';
-  viewMoreBtn.style.display = 'none';
-
-  // add expanded class
-  listItemContainer.classList.add('expanded');
-
-  // display full text
-  jobDescription.textContent = jobDescription.dataset.fullDescription;
-}
-
-async function removeShortlistItem() {
   const removeBtn = event.target;
-  const buttonContainer = removeBtn.parentNode;
-  const jobContainer = buttonContainer.parentNode;
-  const succSub = await submitRemoval(event);
+  const jobContainer = removeBtn.parentNode;
+  const succSub = await submitRemoval(removeBtn);
+
+  // show the button is active
+  removeBtn.classList.add('active-wait');
+
+  await delayPromise;
+  removeBtn.classList.remove('active-wait');
   if (succSub) {
     jobContainer.remove();
     checkEmptyPage();
@@ -116,9 +58,15 @@ async function removeShortlistItem() {
   }
 }
 
-async function submitRemoval(event) {
+function timeoutDelay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const BUTTON_DELAY = 500;
+
+async function submitRemoval(removeBtn) {
   const removal = {};
-  removal.itemId = event.target.dataset.jobid;
+  removal.itemId = removeBtn.dataset.jobid;
   removal.choice = 'shortlist-remove';
   const response = await fetch('user/jobs', {
     method: 'POST',
@@ -126,27 +74,6 @@ async function submitRemoval(event) {
     body: JSON.stringify(removal),
   });
   return response;
-}
-
-function hideDetailedDesc() {
-  // get required DOM elements
-  const viewLessBtn = event.target;
-  const viewMoreBtn = viewLessBtn.previousElementSibling;
-  const removeBtn = viewLessBtn.nextElementSibling;
-  const buttonContainer = viewLessBtn.parentNode;
-  const jobDescription = buttonContainer.previousElementSibling;
-  const listItemContainer = buttonContainer.parentNode;
-
-  // remove expanded class
-  listItemContainer.classList.remove('expanded');
-
-  // truncate overflowing text
-  truncateOverflow(jobDescription.textContent, jobDescription);
-
-  // hide remove button and view less button, show view more button
-  removeBtn.style.display = 'none';
-  viewLessBtn.style.display = 'none';
-  viewMoreBtn.style.display = '';
 }
 
 function checkEmptyPage() {
