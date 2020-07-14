c410a43448d4b76fc92c94c0a363cc25f6115e41
JavaRip <up941226@myport.ac.uk>
2020-07-14 19:58:02 +0100

    :art: + fix shortlisting on like history

--- career-guide/client/js/likehistory.js
+++ career-guide/client/js/likehistory.js
@@ -23,36 +23,50 @@ function displayLikeHistory(jobList) {
   for (const job of jobList) {
     if (job.answer === 'show later') continue;
 
-    // btn-3 is green btn-1 is red
-    const jobChoiceClass = job.answer === 'like' ? 'btn-3' : 'btn-1';
-    const jobChoiceText = job.answer === 'like' ? 'Liked' : 'Disliked';
-    const jobShortlistText = job.shortlist == null ? 'Add to shortlist' : 'Remove from shortlist';
-    const shortlisted = job.shortlist == null ? 'not-shortlisted' : 'shortlisted';
     const jobContainer = document.importNode(template.content, true);
 
+    // display job data
     jobContainer.querySelector('.job-image').src = 'img/' + job.image;
     jobContainer.querySelector('.job-image').alt = job.title_en + 'image';
     jobContainer.querySelector('.list-item-title').textContent = job.title_en;
     jobContainer.querySelector('.job-desc').textContent = job.description_en;
-    jobContainer.querySelector('.job-choice').classList.add(jobChoiceClass);
-    jobContainer.querySelector('.job-choice').textContent = jobChoiceText;
-    jobContainer.querySelector('.job-choice').dataset.jobId = job.id;
-    jobContainer.querySelector('.job-choice').dataset.answer = job.answer;
-    jobContainer.querySelector('.job-choice').addEventListener('click', changeChoice);
-    jobContainer.querySelector('.job-shortlist').dataset.jobId = job.id;
-    jobContainer.querySelector('.job-shortlist').dataset.answer = shortlisted;
-    jobContainer.querySelector('.job-shortlist').textContent = jobShortlistText;
-    jobContainer.querySelector('.job-shortlist').addEventListener('click', changeChoice);
+
+    // get job buttons
+    const jobBtns = getContainerButtons(jobContainer);
+
+    // un-hide required buttons
+    if (job.answer === 'like') jobBtns.likeBtn.style.display = '';
+    if (job.answer === 'dislike') jobBtns.dislikeBtn.style.display = '';
+    if (job.shortlist == null) jobBtns.addToShortlistBtn.style.display = '';
+    if (job.shortlist != null) jobBtns.removeShortlistBtn.style.display = '';
+
+    // add jobId to dataset
+    for (const button in jobBtns) {
+      jobBtns[button].dataset.jobId = job.id;
+    }
 
     listContainer.appendChild(jobContainer);
   }
+}
+
+function getContainerButtons(container) {
+  return {
+    likeBtn: container.querySelector('.job-choice-liked'),
+    dislikeBtn: container.querySelector('.job-choice-disliked'),
+    removeShortlistBtn: container.querySelector('.job-shortlist-remove'),
+    addToShortlistBtn: container.querySelector('.job-shortlist-add'),
+    confirmShortlistBtn: container.querySelector('.job-shortlist-add-confirm'),
+  };
 }
 
 function hideFiltered() {
   const listItems = document.querySelectorAll('.list-item-container');
   const filter = document.querySelector('#history-filter-btns > .selected').dataset.filter;
   for (const container of listItems) {
-    const jobChoice = container.querySelector('.btn-container > .job-choice').dataset.answer;
+    const choiceBtns = getContainerButtons(container);
+
+    // if liked button is being displayed choice for this job is liked
+    const jobChoice = choiceBtns.likeBtn.style.display === '' ? 'like' : 'dislike';
 
     if (filter === 'none') {
       container.style.display = '';
@@ -65,42 +79,38 @@ function hideFiltered() {
 }
 
 async function changeChoice(event) {
-  const jobChoiceBtn = event.target;
   const delayPromise = shared.buttonDelay();
-  jobChoiceBtn.classList.add('active-wait'); // show the button is active
+  const jobBtns = getContainerButtons(event.target.parentElement);
 
+  event.target.classList.add('active-wait'); // show the button is active
   const success = await submitChoiceChange(event);
-
   await delayPromise;
-  jobChoiceBtn.classList.remove('active-wait');
+  event.target.classList.remove('active-wait');
 
   if (success) {
     shared.createToast();
-    const startingChoice = jobChoiceBtn.dataset.answer;
+    const startingChoice = event.target.dataset.answer;
     switch (startingChoice) {
       case 'like':
-        jobChoiceBtn.classList.remove('btn-3');
-        jobChoiceBtn.classList.add('btn-1');
-        jobChoiceBtn.dataset.answer = 'dislike';
-        jobChoiceBtn.textContent = 'Disliked';
+        jobBtns.likeBtn.style.display = 'none';
+        jobBtns.dislikeBtn.style.display = '';
         break;
       case 'dislike':
-        jobChoiceBtn.classList.remove('btn-1');
-        jobChoiceBtn.classList.add('btn-3');
-        jobChoiceBtn.dataset.answer = 'like';
-        jobChoiceBtn.textContent = 'Liked';
+        jobBtns.likeBtn.style.display = '';
+        jobBtns.dislikeBtn.style.display = 'none';
         break;
-      case 'shortlisted':
-        jobChoiceBtn.textContent = 'Add to shortlist';
-        jobChoiceBtn.dataset.answer = 'not-shortlisted';
+      case 'shortlist-remove':
+        jobBtns.addToShortlistBtn.style.display = '';
+        jobBtns.removeShortlistBtn.style.display = 'none';
         break;
-      case 'not-shortlisted':
-        jobChoiceBtn.textContent = 'Remove from shortlist';
-        jobChoiceBtn.dataset.answer = 'shortlisted';
+      case 'shortlist-add':
+        jobBtns.removeShortlistBtn.style.display = '';
+        jobBtns.confirmShortlistBtn.style.display = 'none';
+        jobBtns.addToShortlistBtn.style.display = 'none';
+        jobBtns.likeBtn.style.display = '';
+        jobBtns.dislikeBtn.style.display = 'none';
         break;
     }
-    // Uncomment if you want filter to update when answer is changed
-    // hideFiltered();
   } else {
     document.querySelector('h1').textContent = 'Something went wrong! Please refresh';
   }
@@ -119,15 +129,14 @@ async function submitChoiceChange(event) {
     case 'dislike':
       userInput.choice = 'like';
       break;
-    case 'shortlisted':
+    case 'shortlist-remove':
       userInput.choice = 'shortlist-remove';
       break;
-    case 'not-shortlisted':
+    case 'shortlist-add':
       userInput.choice = 'shortlist-add';
       break;
   }
   const response = await postJobChange(userInput);
-
   if (!response.ok) {
     console.log('Error from server: ' + response.statusText + '. Choice change failed');
   }
@@ -143,18 +152,36 @@ async function postJobChange(userInput) {
   return response;
 }
 
-function addFilterEventListeners() {
+function addEventListeners() {
   for (const button of document.querySelectorAll('#history-filter-btns > button')) {
     button.addEventListener('click', setFilter);
   }
+
+  const choiceSelector = `
+    .job-choice-liked,
+    .job-choice-disliked,
+    .job-shortlist-remove,
+    .job-shortlist-add-confirm`;
+  for (const button of document.querySelectorAll(choiceSelector)) {
+    button.addEventListener('click', changeChoice);
+  }
+
+  for (const button of document.querySelectorAll('.job-shortlist-add')) {
+    button.addEventListener('click', confirmShortlist);
+  }
+}
+
+async function confirmShortlist(event) {
+  const shortlistAddBtn = event.target;
+  const shortlistAddConfirm = event.target.nextElementSibling;
+  shortlistAddBtn.classList.add('active-wait');
+  await shared.buttonDelay();
+  shortlistAddBtn.classList.remove('active-wait');
+  shortlistAddBtn.style.display = 'none';
+  shortlistAddConfirm.style.display = '';
 }
 
 function setFilter(event) {
-  // Commented out because we should allow the user to refresh their filter
-  // if filter button is already selected do nothing
-  // if (event.target.classList.contains('selected')) return;
-
-  // clear selected from previous selected filter
   for (const button of document.querySelectorAll('#history-filter-btns > button')) {
     button.classList.remove('selected');
   }
@@ -168,7 +195,7 @@ async function loadPage() {
   shared.initNavbar();
   displayLikeHistory(await getLikeHistory());
   shared.checkEmptyPage();
-  addFilterEventListeners();
+  addEventListeners();
 
   // hide loading label and show main
   shared.hideLoadingLabel();
