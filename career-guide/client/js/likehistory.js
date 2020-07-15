e0dad271703e871481e7bb825e3c87d6d15185aa
JavaRip <up941226@myport.ac.uk>
2020-07-15 23:11:26 +0100

    make timestamp safari compatible

--- career-guide/client/js/likehistory.js
+++ career-guide/client/js/likehistory.js
@@ -30,7 +30,9 @@ function displayLikeHistory(jobList) {
     jobContainer.querySelector('.job-image').alt = job.title_en + 'image';
     jobContainer.querySelector('.list-item-title').textContent = job.title_en;
     jobContainer.querySelector('.job-desc').textContent = job.description_en;
-    jobContainer.querySelector('.like-history-timestamp').textContent = 'Last changed: ' + formatTimestamp(job);
+    const timeStamp = jobContainer.querySelector('.like-history-timestamp');
+    timeStamp.textContent = job.answer === 'like' ? 'Liked: ' : 'Disliked: ';
+    timeStamp.textContent += formatTimestamp(job);
 
     // get job buttons
     const jobBtns = getContainerButtons(jobContainer);
@@ -51,10 +53,21 @@ function displayLikeHistory(jobList) {
 }
 
 function formatTimestamp(job) {
-  // rtf is RelativeTimeFormat
-  const rtf = new Intl.RelativeTimeFormat('en', { style: 'long' });
-  const jsFormatTimestamp = Date.parse(job.timestamp);
-  const secondsSinceLike = Math.floor((Date.now() - jsFormatTimestamp) / 1000);
+  const jsts = new Date(Date.parse(job.timestamp)); // JavaScriptTimeStamp
+  const secondsSinceLike = Math.floor((Date.now() - jsts) / 1000);
+
+  // safari format safe date as safari does not support Intl
+  if (navigator.userAgent.includes('Safari')) {
+    const year = jsts.getFullYear();
+    // if day or month is only one character (ie 2020/5/4) add leading 0 (ie 2020/05/04)
+    const month = String(jsts.getMonth()).length > 1 ? jsts.getMonth() : '0' + jsts.getMonth();
+    const day = String(jsts.getDate()).length > 1 ? jsts.getDate() : '0' + jsts.getDate();
+    const hours = String(jsts.getHours()).length > 1 ? jsts.getHours() : '0' + jsts.getHours();
+    const mins = String(jsts.getMinutes()).length > 1 ? jsts.getMinutes() : '0' + jsts.getMinutes();
+    return year + '/' + month + '/' + day + ' ' + hours + ':' + mins;
+  }
+
+  const rtf = new Intl.RelativeTimeFormat('en', { style: 'long' }); // rtf is RelativeTimeFormat
   if (secondsSinceLike < 60) {
     return rtf.format(-secondsSinceLike, 'second');
   } else if (secondsSinceLike < 60 * 60) {
@@ -64,9 +77,8 @@ function formatTimestamp(job) {
   } else if (secondsSinceLike < 60 * 60 * 24 * 7) {
     return rtf.format(Math.floor(-secondsSinceLike / (60 * 60 * 24)), 'day');
   } else {
-    return new Intl.DateTimeFormat('en-GB').format(jsFormatTimestamp);
+    return new Intl.DateTimeFormat('en-GB').format(jsts);
   }
-  // test: UPDATE likes SET time_stamp = DATE_SUB(time_stamp, INTERVAL $ DAY) where job_id = $;
 }
 
 function getContainerButtons(container) {
@@ -100,39 +112,50 @@ function hideFiltered() {
 
 async function changeChoice(event) {
   const delayPromise = shared.buttonDelay();
-  const jobBtns = getContainerButtons(event.target.parentElement);
+  const jobBtnsContainer = event.target.parentElement;
+  const jobBtns = getContainerButtons(jobBtnsContainer);
+  const likeHistoryContainer = jobBtnsContainer.parentElement;
+  const timeStamp = likeHistoryContainer.querySelector('.like-history-timestamp');
 
-  event.target.classList.add('active-wait'); // show the button is active
+  // show the clicked button is active while waiting for the server to respond
+  event.target.classList.add('active-wait');
   const success = await submitChoiceChange(event);
   await delayPromise;
   event.target.classList.remove('active-wait');
 
   if (success) {
     shared.createToast();
-    const startingChoice = event.target.dataset.answer;
-    switch (startingChoice) {
-      case 'like':
-        jobBtns.likeBtn.style.display = 'none';
-        jobBtns.dislikeBtn.style.display = '';
-        break;
-      case 'dislike':
-        jobBtns.likeBtn.style.display = '';
-        jobBtns.dislikeBtn.style.display = 'none';
-        break;
-      case 'shortlist-remove':
-        jobBtns.addToShortlistBtn.style.display = '';
-        jobBtns.removeShortlistBtn.style.display = 'none';
-        break;
-      case 'shortlist-add':
-        jobBtns.removeShortlistBtn.style.display = '';
-        jobBtns.confirmShortlistBtn.style.display = 'none';
-        jobBtns.addToShortlistBtn.style.display = 'none';
-        jobBtns.likeBtn.style.display = '';
-        jobBtns.dislikeBtn.style.display = 'none';
-        break;
-    }
+    updateJobBtn(event, jobBtns);
+    timeStamp.textContent = event.target.dataset.answer === 'like'
+      ? 'Disliked: just now'
+      : 'Liked: just now';
   } else {
     document.querySelector('h1').textContent = 'Something went wrong! Please refresh';
+  }
+}
+
+function updateJobBtn(event, jobBtns) {
+  const startingChoice = event.target.dataset.answer;
+  switch (startingChoice) {
+    case 'like':
+      jobBtns.likeBtn.style.display = 'none';
+      jobBtns.dislikeBtn.style.display = '';
+      break;
+    case 'dislike':
+      jobBtns.likeBtn.style.display = '';
+      jobBtns.dislikeBtn.style.display = 'none';
+      break;
+    case 'shortlist-remove':
+      jobBtns.addToShortlistBtn.style.display = '';
+      jobBtns.removeShortlistBtn.style.display = 'none';
+      break;
+    case 'shortlist-add':
+      jobBtns.removeShortlistBtn.style.display = '';
+      jobBtns.confirmShortlistBtn.style.display = 'none';
+      jobBtns.addToShortlistBtn.style.display = 'none';
+      jobBtns.likeBtn.style.display = '';
+      jobBtns.dislikeBtn.style.display = 'none';
+      break;
   }
 }
 
