b89a2c43f8a830a484b0393abf1bbd5c5e57a46b
Jacek Kopecky <jacek@jacek.cz>
2020-11-19 14:05:32 +0000

    add cors for local testing

--- maps-api/index.js
+++ maps-api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const config = require('./config');
 
 const rootApp = express();
+
+// allow local testing of pages
+rootApp.use(cors({ origin: 'http://localhost:8080' }));
+
 const api = require('./api');
 
 const root = config.DEPLOYMENT_ROOT || '/';
